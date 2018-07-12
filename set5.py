65927984dc72eaf35e99f0497aecc912c73292f1
A. Sushmaa Farheen <38107674+shermelee@users.noreply.github.com>
2018-07-12 14:47:33 +0530

    Update set5.py

--- set5.py
+++ set5.py
@@ -1,11 +1,11 @@
-num1=int(input())
-num2=int(input())
-num3=int(input())
-if (num1 >= num2) and (num1 >= num3):
-   largest = num1
-elif (num2 >= num1) and (num2 >= num3):
-   largest = num2
+x=int(input())
+y=int(input())
+z=int(input())
+if (x >= y) and (x >= y):
+   largest = x
+elif (y >= x) and (y >= z):
+   largest = y
 else:
-   largest = num3
+   largest = z
 
 print(largest)
